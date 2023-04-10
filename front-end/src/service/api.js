fd94d89648ff1618f515a8b225d6e9eed6dd0af8
Vinicius <vini_bodra@hotmail.com>
2023-04-10 11:33:48 -0300

    connecting to deployed API

--- front-end/src/service/api.js
+++ front-end/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://delivery-app-backend-blw0.onrender.com/',
 });
 
 export const authLogin = async (login) => {
